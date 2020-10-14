fb908ced0e4b83f13cda702f4b0739bb74c0facc
Nestor T. Amar Jr <72591828+amar-nestorj@users.noreply.github.com>
2020-10-14 18:11:02 +0800

    Add files via upload

---
+++ tryAnimal/tryAnimal/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryAnimal
+{
+    class Animals
+    {
+        public string animalName { get; set; }
+
+        public string diet { get; set; }
+
+        public void newName()
+        {
+            Console.Write("Name your Animal..\n");
+             this.animalName = Console.ReadLine();
+        }
+
+        public void eat()
+        {
+           string food = "Animal Food";
+           Console.WriteLine("Your Animal: " + this.animalName + " drank " + food);
+        }
+
+        public void drink()
+        {
+            string water = "Water";
+            Console.WriteLine("Your Animal: "+ this.animalName +" drank "+ water);
+        }
+
+        public void sleep()
+        {
+            Console.WriteLine("Your Animal: "+ this.animalName +" sleeps..");
+        }
+
+    }
+}
